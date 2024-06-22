ab0aea89a899c8c3c224ef0467a46ba8aba355ab
milkwxter <ethan.r.bates2003@gmail.com>
2024-06-21 23:33:31 -0500

    weapon works

--- ttt2-role_jan/gamemodes/terrortown/entities/weapons/weapon_ttt2_jan_broom.lua
+++ ttt2-role_jan/gamemodes/terrortown/entities/weapons/weapon_ttt2_jan_broom.lua
@@ -92,3 +92,47 @@ function SWEP:PrimaryAttack()
   end
   -- end of primary attack code
 end
+
+-- Override original primary attack
+function SWEP:SecondaryAttack()
+  -- Melee attack code
+  self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+  
+  if not IsValid(self:GetOwner()) then return end
+
+  self:GetOwner():LagCompensation(true)
+
+  local spos = self:GetOwner():GetShootPos()
+  local sdest = spos + (self:GetOwner():GetAimVector() * 70)
+
+  local kmins = Vector(1,1,1) * -10
+  local kmaxs = Vector(1,1,1) * 10
+
+  local tr = util.TraceHull({start=spos, endpos=sdest, filter=self:GetOwner(), mask=MASK_SHOT_HULL, mins=kmins, maxs=kmaxs})
+
+  -- Hull might hit environment stuff that line does not hit
+  if not IsValid(tr.Entity) then
+    tr = util.TraceLine({start=spos, endpos=sdest, filter=self:GetOwner(), mask=MASK_SHOT_HULL})
+  end
+
+  local hitEnt = tr.Entity
+
+  -- Check if we just hit an entity
+  if IsValid(hitEnt) then
+    if SERVER and tr.Hit and tr.HitNonWorld and IsValid(hitEnt) then
+      if hitEnt:GetClass() == "prop_ragdoll" then
+        -- make sure the body is that of a player not a map ragdoll or whatever
+        local corpsePlayer = CORPSE.GetPlayer(hitEnt)
+		    if not IsValid(corpsePlayer) then
+          LANG.Msg(owner, "That is not a player ragdoll! You cannot eat this one.", nil, MSG_MSTACK_WARN)
+          return
+        end
+        
+        -- change DNA for the body
+        hitEnt.killer_sample = { t = 0, killer = nil }
+	   end
+   end
+   self:GetOwner():LagCompensation(false)
+  end
+  -- end of secondary attack code
+end
