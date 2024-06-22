c50a47d56cd308f5f2306be05ad77b00fe8c7a81
milkwxter <ethan.r.bates2003@gmail.com>
2024-06-21 23:16:52 -0500

    Initial role commit

---
+++ ttt2-role_jan/gamemodes/terrortown/entities/weapons/weapon_ttt2_jan_broom.lua
@@ -0,0 +1,94 @@
+if SERVER then
+  AddCSLuaFile()
+end
+
+SWEP.Base = "weapon_tttbase"
+
+if CLIENT then
+  SWEP.ViewModelFOV = 78
+  SWEP.DrawCrosshair = false
+  SWEP.ViewModelFlip = false
+
+  SWEP.EquipMenuData = {
+    type = "item_weapon",
+    name = "Janitor Broom",
+    desc = "Clean up bodies or DNA!"
+  }
+
+  SWEP.Icon = "vgui/ttt/icon_broom"
+
+  function SWEP:Initialize()
+    self:AddTTT2HUDHelp("Clean up a body.", "Clean up DNA on a body.")
+  end
+end
+
+SWEP.Kind = WEAPON_EQUIP2
+SWEP.CanBuy = nil
+
+SWEP.UseHands = true
+SWEP.ViewModel              = "models/weapons/c_arms.mdl"
+SWEP.WorldModel             = "models/props_c17/pushbroom.mdl"
+
+SWEP.AutoSpawnable = false
+SWEP.NoSights = true
+
+SWEP.HoldType = "crowbar"
+SWEP.LimitedStock = true
+
+SWEP.Primary.Recoil = 0
+SWEP.Primary.ClipSize = 0
+SWEP.Primary.DefaultClip = 0
+SWEP.Primary.Automatic = false
+SWEP.Primary.Delay = 1
+SWEP.Primary.Ammo = "none"
+
+SWEP.AllowDrop = false
+
+-- Removes the Broom on death or drop
+function SWEP:OnDrop()
+	self:Remove()
+end
+
+-- Override original primary attack
+function SWEP:PrimaryAttack()
+  -- Melee attack code
+  self:SetNextPrimaryFire( CurTime() + self.Primary.Delay )
+  
+  if not IsValid(self:GetOwner()) then return end
+
+  self:GetOwner():LagCompensation(true)
+
+  local spos = self:GetOwner():GetShootPos()
+  local sdest = spos + (self:GetOwner():GetAimVector() * 70)
+
+  local kmins = Vector(1,1,1) * -10
+  local kmaxs = Vector(1,1,1) * 10
+
+  local tr = util.TraceHull({start=spos, endpos=sdest, filter=self:GetOwner(), mask=MASK_SHOT_HULL, mins=kmins, maxs=kmaxs})
+
+  -- Hull might hit environment stuff that line does not hit
+  if not IsValid(tr.Entity) then
+    tr = util.TraceLine({start=spos, endpos=sdest, filter=self:GetOwner(), mask=MASK_SHOT_HULL})
+  end
+
+  local hitEnt = tr.Entity
+
+  -- Check if we just hit an entity
+  if IsValid(hitEnt) then
+    if SERVER and tr.Hit and tr.HitNonWorld and IsValid(hitEnt) then
+      if hitEnt:GetClass() == "prop_ragdoll" then
+        -- make sure the body is that of a player not a map ragdoll or whatever
+        local corpsePlayer = CORPSE.GetPlayer(hitEnt)
+		    if not IsValid(corpsePlayer) then
+          LANG.Msg(owner, "That is not a player ragdoll! You cannot eat this one.", nil, MSG_MSTACK_WARN)
+          return
+        end
+        
+        -- then delete body cus he eats it
+        hitEnt:Remove()
+	   end
+   end
+   self:GetOwner():LagCompensation(false)
+  end
+  -- end of primary attack code
+end
---
+++ ttt2-role_jan/lua/terrortown/lang/en/janitor.lua
@@ -0,0 +1,10 @@
+local L = LANG.GetLanguageTableReference("en")
+
+-- GENERAL ROLE LANGUAGE STRINGS
+L[JANITOR.name] = "Janitor"
+L["info_popup_" .. JANITOR.name] = [[You are the Janitor! Clean up bodies or DNA with your broom!]]
+L["body_found_" .. JANITOR.abbr] = "They were a Janitor."
+L["search_role_" .. JANITOR.abbr] = "This person was a Janitor!"
+L["target_" .. JANITOR.name] = "Janitor"
+L["ttt2_desc_" .. JANITOR.name] = [[The Janitor needs to use his broom to clean up bodies and DNA.]]
+L["credit_" .. JANITOR.abbr .. "_all"] = "Janitor, you have been awarded {num} equipment credit(s) for your performance."
